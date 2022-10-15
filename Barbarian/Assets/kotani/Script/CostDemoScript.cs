5f657c41a74b3ad6eb75d693fedc1385c08527b9
zerovanisu <mazinari310@gmail.com>
2022-10-15 18:54:19 +0900

    ボタン編集
    クールタイム追加

--- Barbarian/Assets/kotani/Script/CostDemoScript.cs
+++ Barbarian/Assets/kotani/Script/CostDemoScript.cs
@@ -21,13 +21,41 @@ public class CostDemoScript : MonoBehaviour
     private Text CostText;
     [SerializeField]
     private Transform PlayerUnitPrefabParent;
-    
+
     [SerializeField]
     private List<GameObject> PlayerUnitPrefabList = new List<GameObject>();
+
+    [Header("ボタン")]
+    [SerializeField]
+    private Image[] Button_obj;
+
+    [Header("クールタイム(設定用)")]
+    [SerializeField]
+    private float[] CoolTime;
+
+    [Header("クールタイム(実機)")]
+    [SerializeField]
+    private float[] Timebox;
+
+    [Header("出撃が可能か")]
+    [SerializeField]
+    private bool[] Possible;
+
     void Awake()
     {
         nowCost = fastCost;
         CostText.text = nowCost.ToString();
+    }
+
+    void Start()
+    {
+        //出撃のスイッチを処理
+        for (int i = 0; i < Timebox.Length; i++)
+        {
+            Timebox[i] = CoolTime[i];
+
+            Possible[i] = true;
+        }
     }
 
     // Update is called once per frame
@@ -41,25 +69,55 @@ public class CostDemoScript : MonoBehaviour
             {
                 nowCost += 1;
                 CostText.text = nowCost.ToString();
+            }   
+        }
+
+        //各リストのクールタイムを進める
+        for (int i = 0; i < Timebox.Length; i++)
+        {
+            Timebox[i] -= Time.deltaTime;
+
+            Button_obj[i].fillAmount = 1 - (float)Timebox[i] / (float)CoolTime[i];//カウントをボタンに反映
+
+            //クールタイムが進め終わったら
+            if (Timebox[i] <= 0)
+            {
+                Possible[i] = true;
             }
-            
         }
     }
 
+    //出撃ボタンが押されたときの処理
     public void FastPlayerUnitSpawn()
     {
-        //出撃ボタンが押されたときの処理
-        PlayerUnitSpawn(PlayerUnitPrefabList[0]);
+        if(Possible[0])
+        {
+            PlayerUnitSpawn(PlayerUnitPrefabList[0]);
+            Timebox[0] = CoolTime[0];
+            Possible[0] = false;
+        }
     }
+
+    //出撃ボタンが押されたときの処理
     public void SecondPlayerUnitSpawn()
     {
-        //出撃ボタンが押されたときの処理
-        PlayerUnitSpawn(PlayerUnitPrefabList[1]);
+        if (Possible[1])
+        {
+            PlayerUnitSpawn(PlayerUnitPrefabList[1]);
+            Timebox[1] = CoolTime[1];
+            Possible[1] = false;
+        }
     }
+
+    //出撃ボタンが押されたときの処理
     public void ThirdPlayerUnitSpawn()
     {
-        //出撃ボタンが押されたときの処理
-        PlayerUnitSpawn(PlayerUnitPrefabList[2]);
+        if (Possible[2])
+        {
+            PlayerUnitSpawn(PlayerUnitPrefabList[2]);
+            Timebox[2] = CoolTime[2];
+            Possible[2] = false;
+        }
     }
 
     private void PlayerUnitSpawn(GameObject PlayerUnit)
